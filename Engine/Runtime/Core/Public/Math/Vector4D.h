9c4b6de06a93d2fe22e6e795002df73546a628e0
ASOBIBA <86977637+LHBM04@users.noreply.github.com>
2026-01-23 01:00:27 +0900

    Update Vector4D.h

--- Engine/Runtime/Core/Public/Math/Vector4D.h
+++ Engine/Runtime/Core/Public/Math/Vector4D.h
@@ -3,91 +3,154 @@
 
 #include <type_traits>
 
-/**
- * @struct Vector4D
- *
- * @brief 3차원 벡터를 정의합니다.
- */
-template <typename TValue>
-    requires std::is_arithmetic_v<TValue>
-class Vector4D final
+namespace Amuse::Math
 {
-public:
-    Vector4D() noexcept
-        : values(static_cast<TValue>(0), static_cast<TValue>(0))
-    {
-    }
-
-    Vector4D(TValue x_, TValue y_, TValue z_, TValue w_) noexcept
-        : values(x_, y_, z_, w_)
-    {
-    }
-
-    Vector4D(TValue value) noexcept
-        : values(value, value, value, value)
-    {
-    }
-
-    Vector4D(const Vector4D& other_) noexcept
-        : values(other_.values[0], other_.values[1], other_.value[2], other_.value[3])
-    {
-    }
-
-    [[nodiscard]]
-    constexpr TValue operator[](std::size_t index_) const noexcept
-    {
-        return values[index_];
-    }
-
-    [[nodiscard]]
-    constexpr inline TValue GetX() const noexcept
-    {
-        return values[0];
-    }
-
-    inline void SetX(TValue x_) noexcept
-    {
-        values[0] = x_;
-    }
-
-    [[nodiscard]]
-    constexpr inline TValue GetY() const noexcept
-    {
-        return values[1];
-    }
-
-    inline void SetY(TValue y_) noexcept
-    {
-        values[1] = y_;
-    }
-
-    [[nodiscard]]
-    constexpr inline TValue GetZ() const noexcept
-    {
-        return values[2];
-	}
-
-    inline void SetZ(TValue z_) noexcept
-    {
-        values[2] = z_;
-	}
-
-    [[nodiscard]]
-    constexpr inline TValue GetW() const noexcept
-    {
-        return values[3];
-    }
-
-    inline void SetW(TValue w_) noexcept
-    {
-        values[3] = w_;
-	}
-
-private:
     /**
-     * @brief 해당 벡터의 좌표들.
+     * @struct Vector4D
+     *
+     * @brief 3차원 벡터를 정의합니다.
      */
-    TValue values[4];
-};
+    template <typename TValue>
+        requires std::is_arithmetic_v<TValue>
+    struct Vector4D final
+    {
+        explicit Vector4D() noexcept
+            : x(static_cast<TValue>(0))
+            , y(static_cast<TValue>(0))
+            , z(static_cast<TValue>(0))
+        {
+        }
+
+        explicit Vector4D(TValue x_, TValue y_, TValue z_, TValue w_) noexcept
+            : x(x_)
+            , y(y_)
+            , z(z_)
+            , w(w_)
+        {
+        }
+
+        explicit Vector4D(TValue value) noexcept
+            : x(value)
+            , y(value)
+            , z(value)
+            , w(value)
+        {
+        }
+
+        explicit Vector4D(const Vector4D& other_) noexcept
+            : x(other_.x)
+            , y(other_.y)
+            , z(other_.z)
+            , w(other_.w)
+        {
+        }
+
+        [[nodiscard]]
+        constexpr const TValue& operator[](std::size_t index_) const noexcept
+        {
+            switch (index_)
+            {
+                case 0:
+                    return x;
+                case 1:
+                    return y;
+                case 2:
+                    return z;
+                case 3:
+                    return w;
+                default:
+                    static_assert(false, "Invalid index");
+                    break;
+            }
+        }
+
+        [[nodiscard]]
+        constexpr Vector4D<TValue> operator+(const Vector4D<TValue>& other_) const noexcept
+        {
+            return Vector3D<TValue>(x + other_.x, y + other_.y, z + other_.z, w + other_.w);
+		}
+
+        [[nodiscard]]
+        constexpr Vector4D<TValue> operator+=(const Vector4D<TValue>& other_) noexcept
+        {
+            x += other_.x;
+            y += other_.y;
+            z += other_.z;
+            w += other_.w;
+
+            return *this;
+        }
+
+        [[nodiscard]]
+        constexpr Vector4D<TValue> operator-(const Vector4D<TValue>& other_) const noexcept
+        {
+            return Vector3D<TValue>(x - other_.x, y - other_.y, z - other_.z, w - other_.w);
+		}
+
+        [[nodiscard]]
+        constexpr Vector4D<TValue> operator-=(const Vector4D<TValue>& other_) noexcept
+        {
+            x -= other_.x;
+            y -= other_.y;
+            z -= other_.z;
+            w -= other_.w;
+
+            return *this;
+		}
+
+        [[nodiscard]]
+        constexpr Vector4D<TValue> operator*(TValue scalar_) const noexcept
+        {
+            return Vector4D<TValue>(x * scalar_, y * scalar_, z * scalar_, w * scalar_);
+        }
+
+        [[nodiscard]]
+        constexpr Vector4D<TValue> operator*=(TValue scalar_) noexcept
+        {
+            x *= scalar_;
+            y *= scalar_;
+            z *= scalar_;
+            w *= scalar_;
+
+            return *this;
+		}
+
+        [[nodiscard]]
+        constexpr Vector4D operator/(TValue scalar) const
+        {
+            return Vector4D(x / scalar, y / scalar, z / scalar, w / scalar);
+        }
+
+        constexpr Vector4D& operator/=(TValue scalar)
+        {
+            x /= scalar;
+            y /= scalar;
+            z /= scalar;
+            w /= scalar;
+
+            return *this;
+        }
+
+        /**
+         * @brief 해당 벡터의 x 좌표.
+         */
+        TValue x;
+
+        /**
+         * @brief 해당 벡터의 y 좌표.
+         */
+        TValue y;
+
+        /**
+         * @brief 해당 벡터의 z 좌표.
+         */
+        TValue z;
+
+        /**
+         * @brief 해당 벡터의 w 좌표.
+         */
+        TValue w;
+    };
+} // namespace Amuse::Math
 
 #endif // !AMUSE_INCLUDE_VECTOR4D_H
