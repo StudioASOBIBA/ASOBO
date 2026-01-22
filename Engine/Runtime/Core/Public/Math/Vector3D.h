f742bba5841a1dd27bd06ad40778f4bab2eec464
ASOBIBA <86977637+LHBM04@users.noreply.github.com>
2026-01-23 01:00:17 +0900

    Update Vector3D.h

--- Engine/Runtime/Core/Public/Math/Vector3D.h
+++ Engine/Runtime/Core/Public/Math/Vector3D.h
@@ -3,80 +3,140 @@
 
 #include <type_traits>
 
-/**
- * @struct Vector3D
- *
- * @brief 3차원 벡터를 정의합니다.
- */
-template <typename TValue>
-    requires std::is_arithmetic_v<TValue>
-class Vector3D final
+namespace Amuse::Math
 {
-public:
-    Vector3D() noexcept
-        : values(static_cast<TValue>(0), static_cast<TValue>(0), static_cast<TValue>(0))
-    {
-    }
-
-    Vector3D(TValue x_, TValue y_, TValue z_) noexcept
-        : values(x_, y_, z_)
-    {
-    }
-
-    Vector3D(TValue value_) noexcept
-        : values(value_, value_, value_)
-    {
-	}
-
-    Vector3D(const Vector3D& other_) noexcept
-        : values(other_.values[0], other_.values[1], other_.values[2])
-    {
-    }
-
-    [[nodiscard]]
-    constexpr TValue operator[](std::size_t index_) const noexcept
-    {
-        return values[index_];
-    }
-
-    [[nodiscard]]
-    constexpr inline TValue GetX() const noexcept
-    {
-        return values[0];
-    }
-
-    inline void SetX(TValue x_) noexcept
-    {
-        values[0] = x_;
-    }
-
-    [[nodiscard]]
-    constexpr inline TValue GetY() const noexcept
-    {
-        return values[1];
-    }
-
-    inline void SetY(TValue y_) noexcept
-    {
-        values[1] = y_;
-    }
-
-    [[nodiscard]]
-    constexpr inline TValue GetZ() const noexcept
-    {
-        return values[2];
-	}
-
-    inline void SetZ(TValue z_) noexcept
-    {
-        values[2] = z_;
-	}
-
-private:
     /**
-     * @brief 해당 벡터의 좌표들.
+     * @struct Vector3D
+     *
+     * @brief 3차원 벡터를 정의합니다.
      */
-    TValue values[3];
-};
+    template <typename TValue>
+        requires std::is_arithmetic_v<TValue>
+    struct Vector3D final
+    {
+        explicit Vector3D() noexcept
+            : x(static_cast<TValue>(0))
+            , y(static_cast<TValue>(0))
+            , z(static_cast<TValue>(0))
+        {
+        }
+
+        explicit Vector3D(TValue x_, TValue y_, TValue z_) noexcept
+            : x(x_)
+            , y(y_)
+            , z(z_)
+        {
+        }
+
+        explicit Vector3D(TValue value) noexcept
+            : x(value)
+            , y(value)
+            , z(value)
+        {
+        }
+
+        explicit Vector3D(const Vector3D& other_) noexcept
+            : x(other_.x)
+            , y(other_.y)
+            , z(other_.z)
+        {
+        }
+
+        [[nodiscard]]
+        constexpr const TValue& operator[](std::size_t index_) const noexcept
+        {
+            switch (index_)
+            {
+                case 0:
+                    return x;
+                case 1:
+                    return y;
+                case 2:
+                    return z;
+                default:
+                    static_assert(false, "Invalid index");
+                    break;
+            }
+        }
+
+        [[nodiscard]]
+        constexpr Vector3D<TValue> operator+(const Vector3D<TValue>& other_) const noexcept
+        {
+            return Vector3D<TValue>(x + other_.x, y + other_.y, z + other_.z);
+		}
+
+        [[nodiscard]]
+        constexpr Vector3D<TValue> operator+=(const Vector3D<TValue>& other_) noexcept
+        {
+            x += other_.x;
+            y += other_.y;
+            z += other_.z;
+
+            return *this;
+        }
+
+        [[nodiscard]]
+        constexpr Vector3D<TValue> operator-(const Vector3D<TValue>& other_) const noexcept
+        {
+            return Vector3D<TValue>(x - other_.x, y - other_.y, z - other_.z);
+		}
+
+        [[nodiscard]]
+        constexpr Vector3D<TValue> operator-=(const Vector3D<TValue>& other_) noexcept
+        {
+            x -= other_.x;
+            y -= other_.y;
+            z -= other_.z;
+
+            return *this;
+		}
+
+        [[nodiscard]]
+        constexpr Vector3D<TValue> operator*(TValue scalar_) const noexcept
+        {
+            return Vector3D<TValue>(x * scalar_, y * scalar_, z * scalar_);
+        }
+
+        [[nodiscard]]
+        constexpr Vector3D<TValue> operator*=(TValue scalar_) noexcept
+        {
+            x *= scalar_;
+            y *= scalar_;
+            z *= scalar_;
+
+            return *this;
+		}
+
+        [[nodiscard]]
+        constexpr Vector3D operator/(TValue scalar) const
+        {
+            return Vector3D(x / scalar, y / scalar, z / scalar);
+        }
+
+        constexpr Vector3D& operator/=(TValue scalar)
+        {
+            x /= scalar;
+            y /= scalar;
+            z /= scalar;
+
+            return *this;
+        }
+
+        /**
+         * @brief
+         */
+        TValue x;
+
+        /**
+         * @brief
+         */
+        TValue y;
+
+        /**
+         * @brief
+         */
+        TValue z;
+    };
+} // namespace Amuse::Math
 
 #endif // !AMUSE_INCLUDE_VECTOR3D_H
