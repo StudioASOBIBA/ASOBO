9ab8c27f25b17b9779e6c688cf7a870100eb3c62
ASOBIBA <86977637+LHBM04@users.noreply.github.com>
2026-01-29 06:37:28 +0900

    Update Test_Vector2D.cpp
    - 공용체 쓰는 게 괜찮긴 한 듯.

--- Engine/Tests/Test_Vector2D/Test_Vector2D.cpp
+++ Engine/Tests/Test_Vector2D/Test_Vector2D.cpp
@@ -7,24 +7,33 @@
 #include "Math/Vector3D.h"
 #include "Math/Vector4D.h"
 
+using namespace Amuse::Math;
+
 int main(int argc_, char** argv_)
 {
-	Amuse::Math::Vector2D<int> lhs(1, 1);
-	Amuse::Math::Vector2D<int> rhs(2, 3);
+	Vector2D<int> lhs(1, 1);
+	Vector2D<int> rhs(2, 3);
 
-	std::cout << std::format("lhs: ({}, {}) ", lhs[0], lhs[1]);
-	std::cout << std::format("rhs: ({}, {})\n", rhs[0], rhs[1]);
+	std::cout << std::format("lhs: ({}, {})\n", lhs.x, lhs.y);
+	std::cout << std::format("rhs: ({}, {})\n", rhs.x, rhs.y);
 
-	Amuse::Math::Vector2D<int> sum = lhs + rhs;
+	std::cout << std::endl;
+
+	std::cout << std::format("lhs(with indexer): ({}, {})\n", lhs[0], lhs[1]);
+	std::cout << std::format("rhs(with indexer): ({}, {})\n", rhs[0], rhs[1]);
+
+	std::cout << std::endl;
+
+	Vector2D<int> sum = lhs + rhs;
 	std::cout << std::format("sum: ({}, {})\n", sum[0], sum[1]);
 
-	Amuse::Math::Vector2D<int> diff = lhs - rhs;
+	Vector2D<int> diff = lhs - rhs;
 	std::cout << std::format("diff: ({}, {})\n", diff[0], diff[1]);
 
-	Amuse::Math::Vector2D<int> scaled = lhs * 3;
+	Vector2D<int> scaled = lhs * 3;
 	std::cout << std::format("scaled: ({}, {})\n", scaled[0], scaled[1]);
 
-	Amuse::Math::Vector2D<int> divided = rhs / 2;
+	Vector2D<int> divided = rhs / 2;
 	std::cout << std::format("divided: ({}, {})\n", divided[0], divided[1]);
 
 	return 0;
