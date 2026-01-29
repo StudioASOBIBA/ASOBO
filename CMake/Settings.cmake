d2e8a83c2c3c3f84a97c973ef7d1ffa07af3900f
ASOBIBA <86977637+LHBM04@users.noreply.github.com>
2026-01-29 15:28:01 +0900

    Test build on linux
    - WSL로 리눅스 빌드 테스트
    - 일단 CL은 잘 돌아가는 듯

--- CMake/Settings.cmake
+++ CMake/Settings.cmake
@@ -1,34 +1,35 @@
-# Set C standard
+# C 표준 설정
 set(CMAKE_C_STANDARD 17)
 set(CMAKE_C_STANDARD_REQUIRED ON)
 set(CMAKE_C_EXTENSIONS OFF)
 
-# Set C++ standard
+# C++ 표준 설정
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
+# ImGui 수학 연산자 정의 설정
 add_compile_definitions(IMGUI_DEFINE_MATH_OPERATORS)
 
-# Set Unicode definitions
+# Unicode 설정
 add_compile_definitions(UNICODE _UNICODE)
 
-# Set Debug definitions
+# 디버그 빌드일 경우, _DEBUG 매크로 정의
 if(CMAKE_BUILD_TYPE STREQUAL "Debug")
     add_compile_definitions(_DEBUG)
 endif()
 
 # Output directories
-set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
-set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/Library")
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/Library")
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/Binary")
 
-# Platform detection
+# 빌드 플랫폼 감지
 if(WIN32)
     set(AMUSE_TARGET_PLATFORM "Windows")
     add_compile_definitions(AMUSE_PLATFORM_WINDOWS)
 elseif(APPLE)
-    set(AMUSE_TARGET_PLATFORM "macOS")
+    set(AMUSE_TARGET_PLATFORM "MacOS")
     add_compile_definitions(AMUSE_PLATFORM_MACOS)
 elseif(UNIX)
     set(AMUSE_TARGET_PLATFORM "Linux")
